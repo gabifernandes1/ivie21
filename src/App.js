4746daf5f9a06711eb96939fd8524392eada16d6
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-23 21:02:45 -0300

    ?

--- src/App.js
+++ src/App.js
@@ -60,28 +60,42 @@ export default function App() {
 
 		let tel = telefone.replace(/ /g, '').replace('-', '');
 		for (let i in convidados) {
-			if (nome == VIP) {
-				setConvidado(convidados[i]);
-				await axios
-					.post(`${process.env.REACT_APP_URL2}`, {
-						nome: 'vip',
-					})
-					.then(setPagina3(true), setPagina2(false), setLoading(false));
+			if (
+				nome == undefined ||
+				nome == '' ||
+				telefone == '' ||
+				telefone == undefined
+			) {
+				setLoading(false);
+				setNotFound(true);
+				setTimeout(() => {
+					setNotFound(false);
+				}, 3000);
 			} else {
-				if (convidados[i].telefone == tel) {
+				if (nome == VIP) {
 					setConvidado(convidados[i]);
-					setPagina3(true);
-					setPagina2(false);
-					setLoading(false);
+					await axios
+						.post(`${process.env.REACT_APP_URL2}`, {
+							nome: 'vip',
+						})
+						.then(setPagina3(true), setPagina2(false), setLoading(false));
 				} else {
-					setLoading(false);
-					setNotFound(true);
-					setTimeout(() => {
-						setNotFound(false);
-					}, 3000);
+					if (convidados[i].telefone == tel) {
+						setConvidado(convidados[i]);
+						setPagina3(true);
+						setPagina2(false);
+						setLoading(false);
+					} else {
+						setLoading(false);
+						setNotFound(true);
+						setTimeout(() => {
+							setNotFound(false);
+						}, 3000);
+					}
 				}
 			}
 		}
+		setLoading(false);
 	}
 
 	useEffect(() => {
